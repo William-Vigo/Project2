3a570ba1edd86a29820556026a7974e8df2023ca
d3rboyfresh123 <d3rboyfresh123@hotmail.com>
2020-03-31 19:12:45 -0400

    created node class

---
+++ Question6.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init(self, x, y, value):
+        self.val = value
+        self.x = x
+        self.y = y
+        self.visited = False
+        self.neighbors = []
+
+class GridGraph:
+    def addGridNode(self, x: int, y: int , value):
+        pass
